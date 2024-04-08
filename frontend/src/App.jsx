1e37dbb3e5eef4a991c40f07c0186db31ec9ae08
Yebbenbe <lmtpox@gmail.com>
2024-04-07 21:33:51 -0300

    enable TopNavigationBar with TopicList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,16 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
+import TopNavigationBar from './components/TopNavigationBar';
 import PhotoList from './components/PhotoList';
-import TopicList from './components/TopicList';
 
 import './App.scss';
-/*
-const sampleDataForPhotoListItem = new Array(3).fill({
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-});
-*/
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      {/*{sampleDataForPhotoListItem.map((data, index) => (
-        <PhotoListItem key={index} data={data} /> 
-      ))} */}
-            <TopicList />
+
+            <TopNavigationBar />
            <PhotoList/>
     </div>
   );
