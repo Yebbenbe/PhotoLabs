c854493a460e8af4523c3f0a6a0d762881428624
Yebbenbe <lmtpox@gmail.com>
2024-04-07 21:42:00 -0300

    add HomeRoute component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import TopNavigationBar from './components/TopNavigationBar';
-import PhotoList from './components/PhotoList';
+import HomeRoute from 'routes/HomeRoute';
 
 import './App.scss';
 
@@ -9,9 +8,7 @@ import './App.scss';
 const App = () => {
   return (
     <div className="App">
-
-            <TopNavigationBar />
-           <PhotoList/>
+      <HomeRoute />
     </div>
   );
 };
