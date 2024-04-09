b9bd92255f2f1a5b4449359b20eb39c5014fb59b
Yebbenbe <lmtpox@gmail.com>
2024-04-09 17:55:22 -0300

    enable favorites tracking on modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 import PhotoList from '../components/PhotoList';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoFavButton from '../components/PhotoFavButton';
 
 const PhotoDetailsModal = ({ photoData, closeModal, likedPhotos, toggleFavorite, openModal }) => {
-  const { urls, user, location } = photoData;
+  const { id, urls, user, location } = photoData;
   const { username, profile } = user;
   const { city, country } = location;
+
+  const isLiked = likedPhotos.includes(id);
+
+  const handleToggleFavorite = () => {
+    toggleFavorite(id);
+  };
 
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={closeModal}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
-
+      <PhotoFavButton isLiked={isLiked} onClick={handleToggleFavorite} />
       <img className="photo-details-modal__image" src={urls.full} alt={`Photo by ${username}`} />
-
       <div className="photo-details-modal__photographer-details">
         <img className="photo-details-modal__photographer-profile" src={profile} alt={`Profile of ${username}`} />
         <div className="photo-details-modal__photographer-info">
@@ -25,12 +31,10 @@ const PhotoDetailsModal = ({ photoData, closeModal, likedPhotos, toggleFavorite,
       </div>
       <div className="photo-details-modal__images">
         <h2 className="photo-details-modal__header">Similar Photos</h2>
-        <div className="photo-details-modal__images">
           <PhotoList
             likedPhotos={likedPhotos}
             toggleFavorite={toggleFavorite}
             openModal={openModal} />
-        </div>
       </div>
     </div>
   );
